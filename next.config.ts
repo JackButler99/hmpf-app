95ecb17c98c217488e5e5f992094f4b3a86106e3
JackButler99 <ridhokurniawan32@gmail.com>
2025-11-26 22:28:14 +0700

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // kalau pakai image optimization di cloud, uncomment ini:
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
